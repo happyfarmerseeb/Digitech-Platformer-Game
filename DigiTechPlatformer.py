959ba3f14d6d514ceb683ba6b4d486258b9fe3cb
happyfarmerseeb <sebastian.cook302@schools.sa.edu.au>
2024-10-31 10:10:18 +1030

    Object Collision Added, Movement Bugs Fixed

--- DigiTechPlatformer.py
+++ DigiTechPlatformer.py
@@ -6,9 +6,15 @@ print("Hello World") #prints "Hello World"
 pygame.init()
 
 #define some important values (default colours, screen width and screen height, etc.)#
-screen_width = 1200
-screen_height = 500
+screen_width = 1280
+screen_height = 720
 font = pygame.font.SysFont("Calibri", 25)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 
 ##Add ball (player) definitions:##
 ball_xpos = 200
@@ -18,7 +24,15 @@ ball_height = 50
 ball_color = (160, 0, 0)
 ballxchange = 0
 ballychange = 0
+ballvelocity = 10
 direction = "null"
+
+##Add ground definitions:##
+ground_xpos = 300
+ground_ypos = 300
+ground_width = 100
+ground_height = 100
+ground_color = BLACK
 
 
 #Window Settings
@@ -31,33 +45,43 @@ running = True #status of game running
 
 while running:
     # poll for events
+    inputs = 0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             key_input = event.key
             if key_input == pygame.K_LEFT:
-                ballxchange = -10
+                ballxchange = -ballvelocity
             if key_input == pygame.K_RIGHT:
-                ballxchange = 10
+                ballxchange = ballvelocity
             if key_input == pygame.K_UP:
-                ballychange = -10
+                ballychange = -ballvelocity
             if key_input == pygame.K_DOWN:
-                ballychange = 10
-                           
+                ballychange = ballvelocity
+
         if event.type == pygame.KEYUP:
             key_end = event.key
-            if key_end == key_input:
-                ballxchange = 0
-                ballychange = 0
-            elif key_end == pygame.K_LEFT:
-                ballxchange += 10
-            elif key_end == pygame.K_RIGHT:
-                ballxchange -= 10
-            elif key_end == pygame.K_UP:
-                ballychange += 10
-            elif key_end == pygame.K_DOWN:
-                ballychange -= 10
+            if key_end == pygame.K_LEFT:
+                if abs(ballxchange) > ballvelocity:
+                    ballxchange += 2*ballvelocity
+                else:
+                    ballxchange += ballvelocity
+            if key_end == pygame.K_RIGHT:
+                if abs(ballxchange) > ballvelocity:
+                    ballxchange -= 2*ballvelocity
+                else:
+                    ballxchange -= ballvelocity
+            if key_end == pygame.K_UP:
+                if abs(ballychange) > ballvelocity:
+                    ballychange += 2*ballvelocity
+                else:
+                    ballychange += ballvelocity
+            if key_end == pygame.K_DOWN:
+                if abs(ballychange) > ballvelocity:
+                    ballychange -= 2*ballvelocity
+                else:
+                    ballychange -= ballvelocity
 
        #     elif (key_end == pygame.K_LEFT or pygame.K_RIGHT) and (key_input != pygame.K_LEFT or pygame.K_RIGHT):
         #        ballxchange == 0
@@ -73,6 +97,7 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("#0230e4")
     pygame.draw.ellipse(screen, ball_color, (ball_xpos, ball_ypos, ball_width, ball_height))
+    ground = pygame.draw.rect(screen, ground_color, (ground_xpos, ground_ypos, ground_width, ground_height), 0, 0, 0) #ground
     # RENDER YOUR GAME HERE
 
 #Position-responsive ball position label:    ball_xpos-20(((screen_width/2)-ball_xpos)/abs((screen_width/2)-ball_xpos)),ball_ypos-20(((screen_height/2)-ball_ypos)/abs((screen_height/2)-ball_ypos))
@@ -91,6 +116,15 @@ while running:
         elif ball_ypos >= screen_height-ball_height:
             ball_ypos = screen_height-ball_height
 
+#ground collision logic:
+        if ball_xpos+ball_width > ground_xpos and ball_xpos+ball_width < ground_xpos+0.5*ground_width and ball_ypos > ground_ypos-ball_height and ball_ypos < ground_ypos + ground_height:
+            ball_xpos=ground_xpos-ball_width
+        if ball_ypos+ball_height > ground_ypos and ball_ypos+ball_height < ground_ypos+0.5*ground_height and ball_xpos > ground_xpos-ball_width and ball_xpos < ground_xpos + ground_width:
+            ball_ypos=ground_ypos-ball_height
+        if ball_xpos < ground_xpos+ground_width and ball_xpos > ground_xpos+0.5*ground_width and ball_ypos > ground_ypos-ball_height and ball_ypos < ground_ypos + ground_height:
+            ball_xpos=ground_xpos+ground_width
+        if ball_ypos < ground_ypos+ground_height and ball_ypos > ground_ypos+0.5*ground_height and ball_xpos > ground_xpos-ball_width and ball_xpos < ground_xpos + ground_width:
+            ball_ypos=ground_ypos+ground_height
         
         pygame.draw.ellipse(screen, ball_color, (ball_xpos, ball_ypos, ball_width, ball_height))
     
@@ -103,6 +137,6 @@ while running:
 
     pygame.display.update()
 
-    clock.tick(30)  # limits FPS to 30
+    clock.tick(50)  # limits FPS to 50
 
 pygame.quit()
