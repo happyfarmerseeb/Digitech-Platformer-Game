da54dde7f3561beba478ed066ede2cb212dd059e
happyfarmerseeb <sebastian.cook302@schools.sa.edu.au>
2024-11-09 12:29:30 +1030

    Input System Overhaul and Physics Implementation
    Instead of having immediate actions based on inputs, inputs are stored and processed in arrays and then acted on. I also added gravity and stopped it from being open-plane movement

--- DigiTechPlatformer.py
+++ DigiTechPlatformer.py
@@ -14,6 +14,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+gravity = 0.42
 
 ##Add button definitions:##
 button_xpos = 1000
@@ -29,14 +30,27 @@ ball_ypos = 200
 ball_width = 50
 ball_height = 50
 ball_color = (160, 0, 0)
-ballxchange = 0
-ballychange = 0
-ballvelocity = 10
+LastInputs = [False, False, False]
+Inputs = [False, False, False]
+ball_IV = 0
+ball_EV = 0
+airtime = 0
+ball_max_x_velocity = 10
+ball_x_velocity = 0
+ball_y_velocity = 0
+ball_grounded = False
+ball_acceleration = 0
+
+#potentially obsoleted variables
+ball_jump_speed = 12
+ball_jump_frames = 0
+
+
 direction = "null"
 
 ##Add ground definitions:##
 ground_xpos = 100
-ground_ypos = 420
+ground_ypos = 520
 ground_width = 1080
 ground_height = 200
 ground_color = BLACK
@@ -52,48 +66,77 @@ running = True #status of game running
 
 while running:
     # poll for events
-    inputs = 0
+    #below code creates an input array which is read later
+    LastInputs = Inputs
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            key_input = event.key
-            if key_input == pygame.K_LEFT:
-                ballxchange = -ballvelocity
-            if key_input == pygame.K_RIGHT:
-                ballxchange = ballvelocity
-            if key_input == pygame.K_UP:
-                ballychange = -ballvelocity
-            if key_input == pygame.K_DOWN:
-                ballychange = ballvelocity
-
-        if event.type == pygame.KEYUP:
-            key_end = event.key
-            if key_end == pygame.K_LEFT:
-                if abs(ballxchange) > ballvelocity:
-                    ballxchange += 2*ballvelocity
-                else:
-                    ballxchange += ballvelocity
-            if key_end == pygame.K_RIGHT:
-                if abs(ballxchange) > ballvelocity:
-                    ballxchange -= 2*ballvelocity
-                else:
-                    ballxchange -= ballvelocity
-            if key_end == pygame.K_UP:
-                if abs(ballychange) > ballvelocity:
-                    ballychange += 2*ballvelocity
-                else:
-                    ballychange += ballvelocity
-            if key_end == pygame.K_DOWN:
-                if abs(ballychange) > ballvelocity:
-                    ballychange -= 2*ballvelocity
-                else:
-                    ballychange -= ballvelocity
+            if event.key == pygame.K_LEFT:
+                Inputs[0] = True
+            if event.key == pygame.K_RIGHT:
+                Inputs[1] = True
+            if event.key == pygame.K_UP:
+                Inputs[2] = True
+	    
+        if (Inputs[0] == True and Inputs[1] == True):
+            Inputs[0] = LastInputs[1]
+            Inputs[1] = LastInputs[0]
+		
+        if event.type == pygame.KEYUP:         
+            if event.key == pygame.K_LEFT:
+                 Inputs[0] = False
+            if event.key == pygame.K_RIGHT:
+                 Inputs[1] = False
+            if event.key == pygame.K_UP:
+                 Inputs[2] = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             if mouse_pos[0] > button_xpos and mouse_pos[1] > button_ypos and mouse_pos[0] < button_xpos+button_width and mouse_pos[1] < button_ypos+button_height:
-                ballvelocity = random.randint(1,100)
+                ball_max_x_velocity = random.randint(1,100)
+        
+
+    #end of input array write
+    #input array read begin
+    #dealing with x acceleration
+    if Inputs[0] == True:
+        ball_acceleration = -0.5 #makes Left key set leftwards acceleration
+    elif Inputs[1] == True:
+        ball_acceleration = 0.5 #makes right key set rightwards acceleration
+    else:
+        if abs(ball_x_velocity)<1: #if velocity is low enough, the game will just set it to 0
+            ball_acceleration = 0
+            ball_x_velocity = 0
+        else: 
+            ball_acceleration = -0.04*ball_x_velocity #makes player slow down when there are no inputs due to friction with ground
+    #updating x velocity
+    ball_x_velocity += ball_acceleration
+    #if ball exceeds maximum x velocity:
+    if abs(ball_x_velocity) > ball_max_x_velocity:
+        ball_x_velocity = ball_max_x_velocity*(ball_x_velocity/abs(ball_x_velocity))
+
+
+    #dealing with jump inputs (Inputs[2] = True)
+    if Inputs[2] == True:
+        if ball_grounded == True:
+            ball_IV = -15
+            airtime = 0
+        if ball_grounded == False:
+            if airtime >= 75:
+                Inputs[2] = False
+            airtime += 1
+    else:
+        ball_IV += 0.5 
+        if ball_grounded == True:            
+            airtime = 0
+            ball_IV = 0
+        else:
+            airtime += 1
+    
+    #calculate y velocity of ball
+    ball_y_velocity = ball_IV+(airtime*gravity)
+
 
 
        #     elif (key_end == pygame.K_LEFT or pygame.K_RIGHT) and (key_input != pygame.K_LEFT or pygame.K_RIGHT):
@@ -119,9 +162,9 @@ while running:
 #Position-responsive ball position label:    ball_xpos-20(((screen_width/2)-ball_xpos)/abs((screen_width/2)-ball_xpos)),ball_ypos-20(((screen_height/2)-ball_ypos)/abs((screen_height/2)-ball_ypos))
     
     # updates display
-    if ballxchange != 0 or ballychange != 0:
-        ball_xpos += ballxchange
-        ball_ypos += ballychange
+    if ball_x_velocity != 0 or ball_y_velocity != 0:
+        ball_xpos += ball_x_velocity
+        ball_ypos += ball_y_velocity
         if ball_xpos < 0:
             ball_xpos = 0
         elif ball_xpos >= screen_width-ball_width:
@@ -135,12 +178,26 @@ while running:
 #ground collision logic:
         if ball_xpos + ball_width > ground_xpos and ball_xpos + ball_width < ground_xpos + ball_width and ball_ypos > ground_ypos - ball_height and ball_ypos < ground_ypos + ground_height:
             ball_xpos = ground_xpos - ball_width
-        if ball_ypos + ball_height > ground_ypos and ball_ypos + ball_height < ground_ypos + ball_height and ball_xpos > ground_xpos - ball_width and ball_xpos < ground_xpos + ground_width:
+            ball_grounded = True
+            ball_y_velocity = -ball_IV
+
+        elif ball_ypos + ball_height > ground_ypos and ball_ypos + ball_height < ground_ypos + ball_height and ball_xpos > ground_xpos - ball_width and ball_xpos < ground_xpos + ground_width:
             ball_ypos = ground_ypos - ball_height
-        if ball_xpos < ground_xpos + ground_width and ball_xpos > ground_xpos + ground_width - ball_width and ball_ypos > ground_ypos - ball_height and ball_ypos < ground_ypos + ground_height:
+            ball_grounded = True
+            ball_y_velocity = -ball_IV
+
+        elif ball_xpos < ground_xpos + ground_width and ball_xpos > ground_xpos + ground_width - ball_width and ball_ypos > ground_ypos - ball_height and ball_ypos < ground_ypos + ground_height:
             ball_xpos = ground_xpos + ground_width
-        if ball_ypos < ground_ypos + ground_height and ball_ypos > ground_ypos + ground_height - ball_height and ball_xpos > ground_xpos - ball_width and ball_xpos < ground_xpos + ground_width:
+            ball_grounded = True
+            ball_y_velocity = -ball_IV
+
+        elif ball_ypos < ground_ypos + ground_height and ball_ypos > ground_ypos + ground_height - ball_height and ball_xpos > ground_xpos - ball_width and ball_xpos < ground_xpos + ground_width:
             ball_ypos = ground_ypos + ground_height
+            ball_grounded = True
+            ball_y_velocity = -ball_IV
+
+        else:
+            ball_grounded = False
         
         pygame.draw.ellipse(screen, ball_color, (ball_xpos, ball_ypos, ball_width, ball_height))
     
